257d3260c73341cead7fec00b540e9cd7cea0684
mlee03 <Mikyung.Lee@noaa.gov>
2022-08-10 14:58:03 -0400

    add if(do_radiation) code back to physics_driver.F90 that was accidentally deleted.

--- atmos_param/physics_driver/physics_driver.F90
+++ atmos_param/physics_driver/physics_driver.F90
@@ -1850,6 +1850,11 @@ real,  dimension(:,:,:), intent(out)  ,optional :: diffm, difft
 
 !---------------------------------------------------------------------
 
+      if (do_radiation) then
+        radturbten(is:ie,js:je,:) = radturbten(is:ie,js:je,:) + Rad_flux_block%tdt_rad(:,:,:)
+        surf_diff%tdt_rad(is:ie,js:je,:)=Rad_flux_block%tdt_rad(:,:,:) !miz
+      endif
+      
 #ifdef SCM
 ! Option to add SCM radiative tendencies from forcing to Rad_flux_block%tdt_lw
 ! and radturbten
