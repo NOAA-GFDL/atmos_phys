dd9c8626785942fa7c82deca666a35223ce0675c
Mikyung Lee <mikyung.lee@noaa.gov>
2022-08-25 18:23:37 +0000

    Delete compileme.sh

--- compileme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export FC=mpiifort
-export CC=mpiicc
-export F77=mpiifort
-
-export FCFLAGS="`nc-config --flibs` -I`nc-config --includedir` -I/home/Mikyung.Lee/am5phys_cleanup/FMS/fms_intel/include -O0 -g -trackback -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -i4 -nowarn -L/home/Mikyung.Lee/am5phys_cleanup/FMS/fms_intel/lib"
-export LDFLAGS="-L/home/Mikyung.Lee/am5phys_cleanup/FMS/fms_intel/lib"
-export CFLAGS="-O0 -sox -qopenmp -debug minimal `nc-config --cflags`"
