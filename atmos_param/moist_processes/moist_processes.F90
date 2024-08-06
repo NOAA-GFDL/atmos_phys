1d692028320bea05d2ab74a18cf6001da78ede66
Fabien Paulot <Fabien.Paulot@noaa.gov>
2024-08-05 20:52:11 -0400

    remove errouneous wet_nh4no3 diagnostics

--- atmos_param/moist_processes/moist_processes.F90
+++ atmos_param/moist_processes/moist_processes.F90
@@ -1190,14 +1190,13 @@ type(mp_removal_type),     intent(inout) :: Removal_mp
        if (id_wetdms_cmip > 0) used = send_data (id_wetdms_cmip, temp_2d, Time, is,js)
      endif
 
-     if (id_wetdep_NH4NO3 > 0 .or. id_wetnh4_cmip > 0) then
+     if (id_wetnh4_cmip > 0) then
        temp_2d = 0.0
-       if( do_donner_deep ) temp_2d = temp_2d + (18.0/WTMAIR)*(total_wetdep_donner(:,:,nnH4NO3) + &
+       if( do_donner_deep ) temp_2d = temp_2d + (18.0/WTMAIR)*( &
                                                                total_wetdep_donner(:,:,nNH4) )
-       if( do_uw_conv  )    temp_2d = temp_2d + (18.0/WTMAIR)*(total_wetdep_uw(:,:,nNH4NO3) + &
+       if( do_uw_conv  )    temp_2d = temp_2d + (18.0/WTMAIR)*( &
                                                                total_wetdep_uw(:,:,nNH4) )
-       if( doing_prog_clouds )       temp_2d = temp_2d - 0.018*(Removal_mp%ls_wetdep(:,:,nNH4NO3) + Removal_mp%ls_wetdep(:,:,nNH4))
-       if (id_wetdep_NH4NO3 > 0) used = send_data (id_wetdep_NH4NO3, temp_2d, Time, is,js)
+       if( doing_prog_clouds )       temp_2d = temp_2d - 0.018*(Removal_mp%ls_wetdep(:,:,nNH4))
        if (id_wetnh4_cmip   > 0) used = send_data (id_wetnh4_cmip,   temp_2d, Time, is,js)
      endif
 
@@ -2479,7 +2478,7 @@ integer                     :: id_wetdep_cmip
 
      !-------- cmip wet deposition fields  ---------
       do ic = 1, size(cmip_names,1)
-        if (TRIM(cmip_names(ic)) .eq. 'nh4' .and. (nNH4NO3 .eq. NO_TRACER .or. nNH4 .eq. NO_TRACER)) then
+        if (TRIM(cmip_names(ic)) .eq. 'nh4' .and. ( nNH4 .eq. NO_TRACER)) then
           id_wetnh4_cmip = 0; cycle  ! skip when tracers are not in field table
         endif
 
